38daaf2b0dc3b978c2b58522bc255024c6339347
Sirinoks <shinigamoo@gmail.com>
2021-09-12 07:12:36 -0400

    Parsing paragraphs finished
    Also comments on what does what

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const fs = require ('fs')
 const path = "./Sherlock Holmes Selected Stories/The Adventure of the Six Napoleans.txt"
-
+//read the file
 const fullText = String(fs.readFileSync(path, 'utf8'));
 
+//future functionality of choosing the element you want to use
 let element = "p";
 
+//divide text into paragraphs
+const paragraphs = fullText.split(/\n\n/);
+//put them all into an array
+let htmlParagraphsArray = [];
+
+paragraphs.forEach((paragraph) => {
+  htmlParagraphsArray.push(`<${element}>${paragraph}</${element}>`);
+});
+
+//put them all into a single string, every paragraph starts from a new line
+const html = htmlParagraphsArray.join('\n');
+
+//old suggested version
+/*
 const html = fullText
   .split(/\n\n/)
   .map(paragraph =>
@@ -12,4 +27,7 @@ const html = fullText
   )
   .join(' ');
 
-  console.log(html);
+*/
+
+//output
+console.log(html);
