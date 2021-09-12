baf75518b4648190db2c6ccaf6e54d03a280aeac
Sirinoks <shinigamoo@gmail.com>
2021-09-12 00:27:58 -0400

    <p> new line parser

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const fs = require ('fs')
 const path = "/Sherlock Holmes Selected Stories/test.txt"
 
 fs.readdirSync(path);
+
+const html = fullText
+  .split(/\r?\n\r?\n/)
+  .map(para =>
+    `<p>${para.replace(/\r?\n/, ' ')}</p>`
+  )
+  .join(' ');
